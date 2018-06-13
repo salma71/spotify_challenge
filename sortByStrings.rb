ff20ccf021cac1121d94107653490acb0ba2fae6
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-13 10:53:37 -0400

    solve sortByStrings

---
+++ sortByStrings.rb
@@ -0,0 +1,18 @@
+  def sortByStrings(s, t)
+    a = s.split('')
+    b = t.split('')
+    new_str = ''
+    for ele in b do
+        for char in a do 
+            if char == ele
+                new_str += char
+            end
+        end
+    end
+    return new_str
+  end
+
+sortByStrings('good', 'odg')
+sortByStrings('weather', 'therapyw')
+# "oodg"
+# "theeraw"
